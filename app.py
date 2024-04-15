4c3aa09818c8b4f762dba448a7f809b872ac4cdb
ajitbabu2 <166916266+ajitbabu2@users.noreply.github.com>
2024-04-16 00:05:47 +0530

    Update app.py

--- app.py
+++ app.py
@@ -75,7 +75,7 @@ def user_input(user_question):
     # new_db = FAISS.load_local(
     #     "faiss_index", embeddings, allow_dangerous_deserialization=True
     # )
-    new_db = FAISS.load_local("faiss_index", embeddings)
+    new_db = FAISS.load_local("faiss_index", embeddings,allow_dangerous_deserialization=True)
     docs = new_db.similarity_search(user_question)
 
     chain = get_conversational_chain()
